42e0d223f3e9ba6ffd1ce7c4d6b1f0b2162b67ec
Santiago Antich Llarrull <santiantich98@gmail.com>
2023-06-04 23:38:19 -0300

    Agregué el título y el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { Container } from "react-bootstrap";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <Container className="my-5 main">
+        <section className="text-light">
+          <h1 className="text-center display-4">Administrador de colores</h1>
+          <hr />
+        </section>
+      </Container>
+      <footer className="bg-dark text-center text-light py-4">
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
